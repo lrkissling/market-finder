e6748c6138f4e6489837b6795c9e5a4fea78d00e
Lowell Kissling <lrkissling@gmail.com>
2017-05-22 02:06:35 -0700

    first pass at integrating yahoo weather api

--- market_finder.js
+++ market_finder.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
+  // modal dialog that shows details for a Farmer's Market
   $("#detail_dialog").dialog({
     autoOpen: false,
     modal: true,
     width: 800,
     height: 300
   });
+
+  // make USDA API call for market detail data
   $("#markets").on("click", ".details-link", function() {
+    $("#detail_dialog").dialog("option", "title", this.name);
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
@@ -16,6 +20,7 @@ $(document).ready(function() {
   });
 });
 
+// analyze user's keypresses on zipcode input to ensure proper zip codes
 $("#zip_code").on("keydown", function(e) {
   // return true if the key press is numerical
   if (e.which >= 48 && e.which <= 57) {
@@ -29,13 +34,16 @@ $("#zip_code").on("keydown", function(e) {
   return false;
 });
 
+// call findNearbyMarkets and getForecast when zip code is entered, otherwise clear the page.
 $("#zip_code").on("keyup", function() {
   val = this.value;
   if (val.length === 5 && isValidZip(val)) {
     findNearbyMarkets(val);
+    getForecast(val);
   }
   else {
     $("#markets").empty();
+    $("#forecast").empty();
   }
 });
 
@@ -47,6 +55,7 @@ function isValidZip(val) {
   return false;
 }
 
+// query USDA API for markets within the given zip code.
 function findNearbyMarkets(zip) {
   $.ajax({
       type: "GET",
@@ -57,6 +66,7 @@ function findNearbyMarkets(zip) {
   });
 }
 
+// handle the data returned by the USDA API for nearby markets.
 function marketResultsHandler(data) {
   $("#markets").empty();
 
@@ -67,23 +77,57 @@ function marketResultsHandler(data) {
     // remove distance from marketname
     name = name.substring(name.indexOf(" ") + 1);
 
+    // inject HTML into markets list
     $("#markets").append(
       $("<li>").append(
-        $("<span>").append(name)
-      ).append(
-        $("<button id ='" + id + "' class='details-link'>").append("Details")
+        $("<span>").append(name),
+        $("<button id ='" + id + "' name='" + name + "' class='details-link'>").append("Details")
     ));
   }
 }
 
+// handle the data returned by the USDA API for market details.
 function marketDetailHandler(data) {
   $("#detail_dialog").empty();
 
   var addr = data.marketdetails.Address;
   var hours = data.marketdetails.Schedule;
+
+  // inject HTML into market detail dialog
   $("#detail_dialog").append(
     $("<div>").append("Address: " + addr),
     $("<div>").append("Hours: " + hours)
   );
   $("#detail_dialog").dialog("open");
+}
+
+// get forecast from yahoo weather API for the given zip code
+function getForecast(zip) {
+  $.ajax({
+      type: "GET",
+      contentType: "application/json; charset=utf-8",
+      url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D" + zip + ")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
+      dataType: 'jsonp',
+      jsonpCallback: 'forecastResultHandler'
+  });
+}
+
+// handle the data returned by the yahoo weather API
+function forecastResultHandler(data) {
+  $("#forecast").empty();
+  if (data.query.results.channel.location.country == "United States") {
+    var f = data.query.results.channel.item;
+
+    // inject forecast info for next three days in appropriate div.
+    $("#forecast").append($("<h3>").append("Forecast"));
+    for (var i = 0; i < 3; i++) {
+      $("#forecast").append(
+        $("<div>").append(f.forecast[i].day + ":"),
+        $("<ul>").append(
+          $("<li>").append("High: " + f.forecast[i].high + " F"),
+          $("<li>").append("Low: " + f.forecast[i].low + " F"),
+          $("<li>").append("Description: " + f.forecast[i].text)
+      ));
+    }
+  }
 }
